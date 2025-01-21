3ab1288433b5a69bcd26e405d50ce704d6e50f95
Alby-3 <albymanoj525@gmail.com>
2025-01-21 14:23:06 +0530

    Add files via upload

---
+++ student.sh
@@ -0,0 +1,15 @@
+echo "Enter your name"
+read name
+echo "Enter your semester "
+read semester
+echo "Enter your batch"
+read batch 
+echo "Enter your roll no"
+read roll no
+echo " Enter your register number "
+read register number 
+echo "Name :"$name
+echo "semester :"$semester
+echo "batch :"$batch
+echo "roll no:"$roll no
+echo "register number  :"$register number 
